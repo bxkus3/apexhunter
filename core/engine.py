ed890c23f9a2655847e4b0435c0e3f9e7093cb9f
bxkus <xbakussx@gmail.com>
2026-07-18 00:49:57 +0200

    Fix path format for lsass.exe in SYSTEM_PROCESS_PATHS

--- core/engine.py
+++ core/engine.py
@@ -56,7 +56,7 @@ class ScanEngine:
 
     SYSTEM_PROCESS_PATHS = {
         "svchost.exe": ["C:\\Windows\\System32\\", "C:\\Windows\\SysWOW64\\"],
-        "lsass.exe": [r"C:\Windows\System32\"],
+        "lsass.exe": ["C:\\Windows\\System32\\"],
         "services.exe": [r"C:\Windows\System32\"],
         "smss.exe": [r"C:\Windows\System32\"],
         "csrss.exe": [r"C:\Windows\System32\"],
