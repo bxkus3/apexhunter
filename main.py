ae3846e47e3cb143423408820b192aeb6d6155a0
bxkus <xbakussx@gmail.com>
2026-07-18 00:46:31 +0200

    Refactor is_elevated to simplify admin check
    Refactored is_elevated function to use IsUserAnAdmin for checking admin privileges.

--- main.py
+++ main.py
@@ -7,42 +7,16 @@ Handles UAC privilege escalation and application bootstrap.
 import sys
 import os
 import ctypes
-from ctypes import wintypes
 
 
 def is_elevated() -> bool:
     """
-    Determine if the current process token has the Administrator SID.
-    Uses CheckTokenMembership to avoid shell32 dependency where possible.
+    Determine if the current process token has Administrator privileges.
     """
-    SECURITY_NT_AUTHORITY = (0, 0, 0, 0, 0, 5)
-
-    class SID_IDENTIFIER_AUTHORITY(ctypes.Structure):
-        _fields_ = [("Value", ctypes.c_ubyte * 6)]
-
-    sid = wintypes.PSID()
-    sia = SID_IDENTIFIER_AUTHORITY()
-    sia.Value = SECURITY_NT_AUTHORITY
-
-    alloc = ctypes.windll.advapi32.AllocateAndInitializeSid
-    alloc.argtypes = [
-        ctypes.POINTER(SID_IDENTIFIER_AUTHORITY), ctypes.c_byte,
-        wintypes.DWORD, wintypes.DWORD, wintypes.DWORD,
-        wintypes.DWORD, wintypes.DWORD, wintypes.DWORD,
-        wintypes.DWORD, wintypes.DWORD, ctypes.POINTER(wintypes.PSID)
-    ]
-    alloc.restype = wintypes.BOOL
-
-    if not alloc(ctypes.byref(sia), 2, 32, 544, 0, 0, 0, 0, 0, 0, ctypes.byref(sid)):
-        return False
-
     try:
-        member = wintypes.BOOL()
-        if ctypes.windll.advapi32.CheckTokenMembership(None, sid, ctypes.byref(member)):
-            return bool(member.value)
-    finally:
-        ctypes.windll.advapi32.FreeSid(sid)
-    return False
+        return ctypes.windll.shell32.IsUserAnAdmin() != 0
+    except:
+        return False
 
 
 def escalate_uac():
